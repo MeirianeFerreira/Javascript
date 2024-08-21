5d940300948e89873aa6ab2c536086850cff19d8
MeirianeFerreira <meirianesouza02@gmail.com>
2024-08-21 10:13:09 -0300

    Add files via upload

---
+++ javascript/javascript/variaveiseinteporlacao/js/script.js
@@ -0,0 +1,55 @@
+//comentário de uma linha 
+/*comentário 
+de várias 
+linhas*/
+
+//Exibindo um número na tela 
+console.log(2);
+ 
+//exinindo o tipo de dados na tela
+console.log(typeof 2.5);
+
+//declarando constantee imprimindo
+const meunumero =10;
+console.log(meunumero);
+
+//erro ao alterar constante
+const numero =5;
+
+//numero=3;
+console.log(numero)
+
+//string utilizando aspar simples e aspar duplas
+console.log("Estou aprendendo java script");
+console.log('Continue a aprendizagem')
+
+//CONTAGEM DE STRINGS
+const nome = "Carlos Cunha";
+console.log(nome.length);
+
+//string de números
+const valores="589";
+console.log(valores);
+console.log(typeof valores)
+
+//estuda tabela aski Erika
+// Pular uma linha da string
+console.log(" Vou pular a linha \n R$ 50,00");
+//Espaçamento entre as strings
+console.log("Espaçamento \tna string");
+
+//variaveis var e left var para escopo global -let para escopo local
+var numero1 = 5
+numero1= 3;
+console.log(numero1);
+let numero2 = 10;
+numero2 = 3;
+console.log(numero2);
+
+//interpolação
+const sobrenome= "Cunha";
+console.log(`Olá, ${sobrenome}`);
+const idade= 34;
+console.log(`sua idade é ${idade} anos`);
+console.log( `Seu sobrenome é ${sobrenome}, sua idade é ${idade}`);
+
