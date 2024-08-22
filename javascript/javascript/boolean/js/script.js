119e311aec4c8652d86eeef3e1bd76ae8b162c66
MeirianeFerreira <meirianesouza02@gmail.com>
2024-08-22 11:08:54 -0300

    Add files via upload

---
+++ javascript/javascript/boolean/js/script.js
@@ -0,0 +1,10 @@
+//Boolean verdadeiro e falso - literal
+const vencedor = false;
+const perdedor = true;
+console.log(vencedor);
+console.log( typeof vencedor);
+
+//Boolean com operação relacional
+const maioridade = 18;
+const maiordeIdade = maioridade > 17;
+console.log(maiordeIdade);
---
+++ javascript/javascript/objetodoconsole/js/script.js
@@ -0,0 +1,17 @@
+//Exibindo uma mesnasagem de aviso na console
+console.warn("Atenção: Aviso!");
+
+//Exibindo uma mesnasagem de erro
+console.error("Ocorreu error");
+
+//Exibindo uma mesnasagem infoemativa na console
+console.info("Informativa");
+
+// Criando um grupo de mensagem na console
+console.group("Mensagem de teste");
+console.log("Atente-se ao passo 1");
+console.log("Execute o passo 2");
+console.groupEnd();
+
+//Limpar o console
+//Usar o: console.clear();
